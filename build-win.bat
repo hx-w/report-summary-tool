f77ff55e1f291f0880302755fb4b802e1de152ba
hx-w <herixth@163.com>
2021-10-15 11:01:36 +0800

    static

--- build-win.bat
+++
@@ -1,3 +0,0 @@
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-pyinstaller -F -i static/icon.ico main.py
-rmdir build/
---
+++ build.sh
@@ -0,0 +1,10 @@
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+rm -rf build/ dist/
+rm -f main.spec
+pyinstaller -F -i static/icon.ico main.py
+rm -rf build/
+rm -f main.spec
+
+if [ -f "dist/main.exe" ]; then
+    cp config.yml dist/main.exe env/06_工作周报/
+fi
